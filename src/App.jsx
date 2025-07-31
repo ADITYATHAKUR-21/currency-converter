4431014515366fa7f28704035b54a275cbc9d28f
Aditya Kumar <adityakr123muz@gmail.com>
2025-07-31 13:16:18 +0530

    Added components in src

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat bg-center '
+     style={{ backgroundImage: `url(https://images.pexels.com/photos/33175665/pexels-photo-33175665.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)`}} >
+      <h1>Currancy Converter</h1>
+    </div>
   )
 }
 
---
+++ src/components/InputBox.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const InputBox = ({
+   lable,
+   onAmountChange,
+   onCurrencyChange,
+   currencyOption = [],
+   selectedCurrency = `USD`
+}) => {
+  return (
+    <div>InputBox</div>
+  )
+}
+
+export default InputBox
